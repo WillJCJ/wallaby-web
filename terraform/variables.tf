2c5890c2569bf2b156a62cf712ab22430c4f27e5
William Johnson <will.johnson@hippodigital.co.uk>
2026-04-01 20:46:46 +0100

    Bin off everything to start again

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  s3_origin_id = "S3-wallaby-web-static"
-  my_domain    = "wallabyfest.co.uk"
-
-  lambda_src_dir    = "../awslambda"
-  lambda_deploy_dir = "deploy"
-
-  static_directory = "../static"
-}
