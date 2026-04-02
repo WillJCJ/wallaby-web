266ce08b82f1cf03357a7de9f5178bdbb6a7b086
William Johnson <will.johnson@hippodigital.co.uk>
2026-04-02 19:40:56 +0100

    Bin off terraform just manage with wrangler

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cloudflare_nameservers" {
-  description = "These are now set in Route 53 automatically"
-  value       = cloudflare_zone.wallabyfest.name_servers
-}
-
-output "worker_url" {
-  value = "https://${local.my_domain}"
-}
